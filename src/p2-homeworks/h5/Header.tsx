f2a711fecf22054a64ffd44471d929dfc25957c6
ipavka <ipavka@ya.ru>
2022-03-05 19:15:34 +0300

    HM_5

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,10 +1,28 @@
 import React from 'react'
+import {NavLink} from "react-router-dom";
+import navStyle from './Header.module.css';
 
 function Header() {
+
     return (
         <div>
-            // add NavLinks
-
+            <nav>
+                <div className={navStyle.main}>
+                    <div>
+                        <NavLink className={(data) => data.isActive ? navStyle.active__link : ''}
+                                 to={'/pre-junior'}>pre-junior</NavLink>
+                    </div>
+                    <div>
+                        <NavLink className={(data) => data.isActive ? navStyle.active__link : ''}
+                                 to={'/junior'}>junior</NavLink>
+                    </div>
+                    <div>
+                        <NavLink className={(data) => data.isActive ? navStyle.active__link : ''}
+                                 to={'/junior-plus'}>junior+</NavLink>
+                    </div>
+                    <div className={navStyle.hide}>Menu</div>
+                </div>
+            </nav>
         </div>
     )
 }
---
+++ src/p2-homeworks/h5/pages/Junior.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Junior = () => {
+    return (
+        <div>
+            <h3 style={{textAlign: "center"}}>Junior</h3>
+        </div>
+    );
+};
+
